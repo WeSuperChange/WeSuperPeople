6e0bc4f81e3b691f94289b116adf8ab9e23ae704
Fabian2411 <fabian.backhaus1@gmx.de>
2021-11-09 17:46:29 +0100

    fabian: share+css

--- App/src/components/Share.jsx
+++ App/src/components/Share.jsx
@@ -1,20 +1,14 @@
 import React from "react";
+import styles from '../assets/css/Share.module.css';
 
 const Share = () => {
     return (
-        <div>
-            <p>Share with friends</p>
-            <ul>
-                <li>
-                    <a href="https://www.facebook.com"></a>
-                </li>
-                <li>
-                    <a href="https://www.twitter.com"></a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com"></a>
-                </li>
-            </ul>
+        <div className={styles.share}>
+            <p className={styles.shareFriends}>Share with friends</p>
+            <img className={styles.icon} src="/img/facebook_icon.png" alt="" />
+            <img className={styles.icon} src="/img/twitter_icon.png" alt="" />
+            <img className={styles.icon} src="/img/instagram_icon.png" alt="" />
+            <img className={styles.icon} src="/img/link_icon.png" alt="" />
         </div>
     );
 };
